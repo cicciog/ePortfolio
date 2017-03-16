5c1e98de4c4f25ede92cb9078a6d9010c62b88f7
cicciog <f.gualtieri@live.it>
2017-03-17 00:54:59 +0100

    ePortfolio first prototipe

---
+++ ePortfolio/assets/js/ModifyWorkExperience.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+    $(".item").click(function (event) {
+        var id = event.target.id;
+        var number = id.slice(7, 8);
+
+        var oOccupation = $('#job-titleW'+number).text();
+        var oTime = $('#timeW'+number).text();
+        var oDateTo = $('#detailsW'+number).text();
+        var oOrganisation = $('#companyW'+number).text();
+
+        $('#occupationW').val(oOccupation);
+        $('#datefromW').val(oTime.slice(0,10));
+        $('#datetoW').val(oTime.slice(0,10));
+        $('#organisationW').val(oOrganisation);
+    });
+});
+
+
+
+

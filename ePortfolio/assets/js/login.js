cea487fd921935b122353b91fdb4e22f433ac092
cicciog <f.gualtieri@live.it>
2017-03-22 18:11:08 +0100

    clean

--- ePortfolio/assets/js/login.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $('#login-btn').click(function () {
-        if (($('#userLogin').val() === 'user') && ($('#passwordLogin').val() === 'password')) {
-            $(window.location).attr('href', 'joboffers - showcase.html');
-        }else{
-            $('#errorLogin').text('Try again, username o password is not valid!');
-        }
-    });
-});
